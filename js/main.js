6a4462a3db8181316b9bbe1b5608c153f772e314
Mailen <54008452+MailenKleinman@users.noreply.github.com>
2021-01-08 02:44:42 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@
 (function ($) {
     "use strict";
 
+
     /*==================================================================
-    [ Validate after type ]*/
-    $('.validate-input .input100').each(function(){
+    [ Focus Contact2 ]*/
+    $('.input100').each(function(){
         $(this).on('blur', function(){
-            if(validate(this) == false){
-                showValidate(this);
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
             }
             else {
-                $(this).parent().addClass('true-validate');
+                $(this).removeClass('has-val');
             }
         })    
     })
@@ -37,11 +38,10 @@
     $('.validate-form .input100').each(function(){
         $(this).focus(function(){
            hideValidate(this);
-           $(this).parent().removeClass('true-validate');
         });
     });
 
-     function validate (input) {
+    function validate (input) {
         if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
             if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
@@ -58,30 +58,44 @@
         var thisAlert = $(input).parent();
 
         $(thisAlert).addClass('alert-validate');
-
-        $(thisAlert).append('<span class="btn-hide-validate">&#xf135;</span>')
-        $('.btn-hide-validate').each(function(){
-            $(this).on('click',function(){
-               hideValidate(this);
-            });
-        });
     }
 
     function hideValidate(input) {
         var thisAlert = $(input).parent();
+
         $(thisAlert).removeClass('alert-validate');
-        $(thisAlert).find('.btn-hide-validate').remove();
     }
-    
     
 
 })(jQuery);
 
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+var $firstButton = $(".first"),
+  $secondButton = $(".second"),
+  $input = $("input"),
+  $name = $(".name"),
+  $more = $(".more"),
+  $yourname = $(".yourname"),
+  $reset = $(".reset"),
+  $ctr = $(".container");
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
+$firstButton.on("click", function(e){
+  $(this).text("Saving...").delay(900).queue(function(){
+    $ctr.addClass("center slider-two-active").removeClass("full slider-one-active");
+  });
+  e.preventDefault();
+});
+
+$secondButton.on("click", function(e){
+  $(this).text("Saving...").delay(900).queue(function(){
+    $ctr.addClass("full slider-three-active").removeClass("center slider-two-active slider-one-active");
+    $name = $name.val();
+    if($name == "") {
+      $yourname.html("Anonymous!");
+    }
+    else { $yourname.html($name+""); }
+  });
+  e.preventDefault();
+});
+
+// copy
+balapaCop("Step by Step Form", "#999");
